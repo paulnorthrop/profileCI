08cc64b0f35a4299803d642b4ff9f95219a46146
Paul Northrop <p.northrop@ucl.ac.uk>
2025-06-01 00:33:52 +0100

    Document the package

--- R/profileCI-package.R
+++ R/profileCI-package.R
@@ -1,9 +1,18 @@
 #' profileCI: Profiling a Log-Likelihood to Calculate a Confidence Interval
 #'
-#' Describe
+#' Provides tools for profiling a user-supplied log-likelihood function to
+#' calculate confidence intervals for model parameters. Speed of computation
+#' can be improved by adjusting the step sizes in the profiling and/or starting
+#' the profiling from limits based on the approximate large sample normal
+#' distribution for the maximum likelihood estimator of a parameter. The
+#' accuracy of the limits can be set by the user. A plot method visualises the
+#' log-likelihood and confidence interval. Only convex log-likelihoods are
+#' supported, that is, disjoint confidence intervals will not be found.
 #'
-#' @details The main function is \code{\link{profileCI}}.
+#' @details The main function is [`profileCI`], which profiles the
+#'   log-likelihood, with respect to one parameter at a time.
 #' @docType package
 #' @aliases profileCI-package
+#' @seealso [`profileCI`] and [`plot.profileCI`].
 #' @import stats
 "_PACKAGE"
