40e296d67f1399fb3a414b693f1ffc01c42b6a01
Paul Northrop <p.northrop@ucl.ac.uk>
2025-05-31 18:05:13 +0100

    Add test

---
+++ tests/testthat/test-profileCI-methods.R
@@ -0,0 +1,14 @@
+# Check plot.profileCI()
+
+my_prof <- profileCI(glm.D93, loglik = poisson_loglik, profile = TRUE,
+                     mult = 32, faster = TRUE)
+
+interval1 <- plot(my_prof, parm = 1)
+interval2 <- plot(my_prof, parm = "outcome2")
+
+test_that("Symmetric intervals for Poisson GLM", {
+  expect_equal(interval1, my_prof[1, ])
+})
+test_that("Symmetric intervals for Poisson GLM", {
+  expect_equal(interval2, my_prof["outcome2", ])
+})
