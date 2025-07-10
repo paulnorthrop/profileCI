1929408742a9bd72421c816e3601b121bab42690
Paul Northrop <p.northrop@ucl.ac.uk>
2025-07-11 00:08:02 +0100

    Typo

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -4,7 +4,7 @@
 counts <- c(18,17,15,20,10,20,25,13,12)
 outcome <- gl(3, 1, 9); treatment <- gl(3, 3)
 glm.D93 <- glm(counts ~ outcome + treatment, family = poisson())
-# Fir the intercept only model
+# Fit the intercept only model
 glm.0 <- glm(counts ~ 1, family = poisson())
 
 poisson_loglik <- function(par) {
