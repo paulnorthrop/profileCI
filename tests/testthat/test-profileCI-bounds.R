ebbb45ce16659f6e943dbb0079b839565d90f447
Paul Northrop <p.northrop@ucl.ac.uk>
2025-07-11 14:07:50 +0100

    Speed up the tests

--- tests/testthat/test-profileCI-bounds.R
+++ tests/testthat/test-profileCI-bounds.R
@@ -4,7 +4,8 @@
 ## faster = FALSE
 
 # Bounded search
-prof <- profileCI(glm.D93, faster = FALSE, lb = rep(-0.6, 5), ub = rep(3.2, 5))
+prof <- profileCI(glm.D93, parm = 1:4, faster = FALSE,
+                  lb = rep(-0.6, 4), ub = rep(3.2, 4))
 # Elements [2, 1], [3, 1] and [1, 2] of prof should become NA in prof_bounded
 theNAs <- c(prof[2, 1], prof[3, 1], prof[1, 2])
 
@@ -15,7 +16,8 @@ test_that("Bounds lb and ub work, faster = FALSE", {
 ## faster = TRUE
 
 # Bounded search
-prof <- profileCI(glm.D93, faster = TRUE, lb = rep(-0.6, 5), ub = rep(3.2, 5))
+prof <- profileCI(glm.D93, parm = names(coef(glm.D93))[1:4], faster = TRUE,
+                  lb = rep(-0.6, 4), ub = rep(3.2, 4))
 # Elements [2, 1], [3, 1] and [1, 2] of prof should become NA in prof_bounded
 theNAs <- c(prof[2, 1], prof[3, 1], prof[1, 2])
 
