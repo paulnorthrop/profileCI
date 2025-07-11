3d4d52dc8938620f1dd94cdb8574111d264e65e7
Paul Northrop <p.northrop@ucl.ac.uk>
2025-07-11 13:50:11 +0100

    Make tests faster

--- tests/testthat/test-profileCI-bounds.R
+++ tests/testthat/test-profileCI-bounds.R
@@ -3,33 +3,22 @@
 
 ## faster = FALSE
 
-# Unbounded search
-prof <- profileCI(glm.D93, faster = FALSE)
 # Bounded search
-prof_bounded <- profileCI(glm.D93, faster = FALSE,
-                          lb = rep(-0.6, 5), ub = rep(3.2, 5))
+prof <- profileCI(glm.D93, faster = FALSE, lb = rep(-0.6, 5), ub = rep(3.2, 5))
 # Elements [2, 1], [3, 1] and [1, 2] of prof should become NA in prof_bounded
-prof[2, 1] <- NA
-prof[3, 1] <- NA
-prof[1, 2] <- NA
+theNAs <- c(prof[2, 1], prof[3, 1], prof[1, 2])
 
 test_that("Bounds lb and ub work, faster = FALSE", {
-  expect_equal(prof, prof_bounded, ignore_attr = TRUE)
+  expect_true(all(is.na(theNAs)))
 })
 
 ## faster = TRUE
 
-# Unbounded search
-prof <- profileCI(glm.D93, faster = TRUE)
 # Bounded search
-prof_bounded <- profileCI(glm.D93, faster = TRUE,
-                          lb = rep(-0.6, 5), ub = rep(3.2, 5))
+prof <- profileCI(glm.D93, faster = TRUE, lb = rep(-0.6, 5), ub = rep(3.2, 5))
 # Elements [2, 1], [3, 1] and [1, 2] of prof should become NA in prof_bounded
-prof[2, 1] <- NA
-prof[3, 1] <- NA
-prof[1, 2] <- NA
+theNAs <- c(prof[2, 1], prof[3, 1], prof[1, 2])
 
 test_that("Bounds lb and ub work, faster = TRUE", {
-  expect_equal(prof, prof_bounded, ignore_attr = TRUE)
+  expect_true(all(is.na(theNAs)))
 })
-
